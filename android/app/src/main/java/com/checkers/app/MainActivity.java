e42623646fc2c8dc33a9c2e437f887c4508e839e
mikhailkaryamin <karyamin@uchi.ru>
2025-03-29 22:36:56 +0300

    Add Android platform support

---
+++ android/app/src/main/java/com/checkers/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.checkers.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.checkers.app',
+  appName: 'Шашки',
+  webDir: 'dist'
+};
+
+export default config;
